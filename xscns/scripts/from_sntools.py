012e814c69d2e462b042c99aeee8b0f189db051b
Jost Migenda <jost.migenda@kcl.ac.uk>
2025-10-13 23:23:15 +0100

    clean up script; move user-settable values to top

--- xscns/scripts/from_sntools.py
+++ xscns/scripts/from_sntools.py
@@ -2,7 +2,7 @@
 from_sntools.py
 
 Script to generate a GLoBES cross section file from a cross section implemented
-in sntools (https://github.com/JostMigenda/sntools).
+in sntools (https://github.com/SNEWS2/sntools).
 To use this script:
   1) Install sntools. (See link above for instructions.)
   2) If necessary, adjust the lines marked with a "TODO" comment below
@@ -11,51 +11,57 @@ To use this script:
 from math import log10
 from numpy import linspace
 from scipy import integrate
-from sntools.interaction_channels import o16eb as c  # TODO: select interaction channel
 
-flv = 'eb'  # TODO: select flavor of interacting neutrino from ['e', 'eb', 'x', 'xb']
 
-try:
-    # sntools v1.0 and later
-    c = c.Channel(flv)
-except AttributeError:
-    # sntools v0.7.4 or earlier
-    import builtins
-    builtins._flavor = flv
+##### TODO: adjust variables in this block as necessary #####
 
-    try:
-        _ = c._opts(20)["points"]
-    except AttributeError:
-        def _opts(eNu, *args):
-            return {'points': []}
-        c._opts = _opts
+# import the desired interaction channel from sntools
+import sntools.interaction_channels.o16eb as channel
 
+# flavor of interacting neutrino ('nue', 'nuebar', 'numu', 'numubar', 'nutau', 'nutaubar')
+flv = 'nuebar'
+
+# name of output file
+output_file = f'xs_{flv}_O16_Suzuki2018.dat'
+
+# comments to be written at the top of the output file
+preamble = ("# Electron antineutrino-oxygen charged-current cross section 5MeV-100MeV (10^-38 cm^2/GeV)\n"
+            "# Based on arXiv:1807.02367 (calculations) and arXiv:1809.08398 (fit).\n")
+
+
+##### No need to change anything below this line #####
+
+sntools_flavors = {'nue': 'e', 'numu': 'x', 'nutau': 'x', 'nuebar': 'eb', 'numubar': 'xb', 'nutaubar': 'xb'}
+
+c = channel.Channel(sntools_flavors[flv])
 
 def xs(eNu):
-    eE_min, eE_max = c.bounds_eE(eNu)
+    try:
+        eE_min, eE_max = c.bounds_eE(eNu)
+    except UnboundLocalError:
+        return 0  # if eNu is below energy threshold of the interaction
+    
     if eNu >= c.bounds_eNu[0]:
         xs_in_natural_units = integrate.quad(lambda eE: c.dSigma_dE(eNu, eE), eE_min, eE_max, points=c._opts(eNu)["points"])[0]
     else:
         xs_in_natural_units = 0
     cm2mev = 5.067731E10  # conversion factor derived from hbar*c = 197 MeV*fm = 1
     xs_in_cm2 = xs_in_natural_units / cm2mev**2
-    return xs_in_cm2
-
-
-def print_xs(eNu):
     e_in_GeV = eNu / 1000
-    return xs(eNu) * 1e38 / e_in_GeV
+    return xs_in_cm2 * 1e38 / e_in_GeV
 
 
-with open('xscns/xs_nuebar_O16_Suzuki2018.dat', 'w') as outfile:  # TODO: update file name
-    outfile.write("# Electron antineutrino-oxygen charged-current cross section 5MeV-100MeV (10^-38 cm^2/GeV)\n")  # TODO: update description
-    outfile.write("# Based on arXiv:1807.02367 (calculations) and arXiv:1809.08398 (fit).\n")  # TODO: update reference
+with open(output_file, 'w') as outfile:
+    outfile.write(preamble)
     outfile.write("# log(energy in GeV)   nu_e       nu_mu      nu_tau       nu_e_bar    nu_mu_bar  nu_tau_bar\n\n")
 
     for log10eNu in linspace(log10(0.005), log10(0.100), num=1001):
         eNu = 10**log10eNu * 1000  # convert log10(GeV) to MeV
-        try:
-            val = print_xs(eNu)
-        except:
-            val = 0  # if eNu is below energy threshold of the interaction
-        outfile.write(f"{log10eNu:^+20.6f} {0:^10} {0:^10} {0:^10} {val:^15.5e} {0:^10} {0:^10}\n")  # TODO: swap columns if necessary
+        val = xs(eNu)
+        entry = f"{log10eNu:^+20.6f}"
+        for f in sntools_flavors.keys():
+            if f == flv:
+                entry += f" {val:^15.5e}"
+            else:
+                entry += f" {0:^10}"
+        outfile.write(f"{entry}\n")
