5a9dbf8adf79cbc8b3bc9dfb1939c900f9f607e8
hbjamin <hbjamin@sas.upenn.edu>
2026-03-17 17:24:36 -0400

    bug fixes

--- smearing_code/run_smears_water.C
+++ smearing_code/run_smears_water.C
@@ -3,7 +3,6 @@ void run_smears_water() {
     // TString res = "0.10";
     TString res = "unit_matrix";
 
-
     smear(config, "ibd", res);
     smear(config, "nue_e", res);
     smear(config, "nuebar_e", res);
@@ -11,9 +10,6 @@ void run_smears_water() {
     smear(config, "numubar_e", res);
     smear(config, "nutau_e", res);
     smear(config, "nutaubar_e", res);
-    smear(config, "nue_O16", res);
-    smear(config, "nue_O16_2", res);
-    smear(config, "nue_O16_Newton", res);
     smear(config, "nuebar_O16", res);
     smear(config, "nc_nue_O16", res);
     smear(config, "nc_nuebar_O16", res);
@@ -21,5 +17,9 @@ void run_smears_water() {
     smear(config, "nc_numubar_O16", res);
     smear(config, "nc_nutau_O16", res);
     smear(config, "nc_nutaubar_O16", res);
+    smear(config, "nue_O16", res);
+    smear(config, "nue_O16_2", res);
+    smear(config, "nue_O16_Suzuki2018", res);
+    smear(config, "nue_O16_Newton", res);
 }
 
